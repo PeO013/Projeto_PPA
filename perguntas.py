eccc25a4b361b65ff21399c1d072a90a18465fb3
PeO013 <118534470+PeO013@users.noreply.github.com>
2023-10-06 10:54:09 -0300

    Update perguntas.py

---
+++ perguntas.py
@@ -0,0 +1,184 @@
+perguntas = [ """Qual sua idade? 
+1) Entre 14 e 15 anos
+2) De 16 a 17 anos
+3) De 18 a 19 anos
+4) De 20 a 21 anos 
+5) 22 anos ou mais
+""",
+"""Com qual gênero você se identifica? 
+1- Feminino
+2- Masculino
+3- Pessoa não binária
+4- Outro.
+""",
+"""Qual sua cor ou raça/etnia? 
+1) Branco
+2) Pardo
+3) Preto
+4) Indígena
+5) Amarelo
+6) Não sei
+""",
+"""A moradia onde você habita é:
+1) Própria
+2) Cedida
+3) Alugada
+4) Financiada 
+""",
+"""Sua casa está localizada em:
+1) Zona rural
+2) Zona Urbana 
+""",
+"""Você mora em:
+1) Comunidade indígena
+2) Comunidade quilombola
+3)Cidade circunvizinha à Valença 
+""",
+"""Qual o nível de escolaridade do seu pai?
+1)Analfabeto
+2) Fund. Incompleto
+3)Fund. Completo
+4)Médio Incompleto
+5)Médio Completo
+6)Superior Incompleto
+7) Superior Completo
+8)Não Sei
+""",
+"""Qual o nível de escolaridade da sua mãe? 
+1) Analfabeto
+2) Fund. Incompleto
+3) Fund. Completo
+4) Médio Incompleto
+5) Médio Completo
+6) Superior Incompleto
+7) Superior Completo
+8) Não Sei
+""",
+"""Onde você cursou o ensino fundamental? 
+1)Integralmente em Escola Pública
+2) Integralmente em Escola Particular
+3) Integralmente em Escola Particular com bolsa de estudos
+4) Maior parte em Escola Pública
+5) Maior parte em Escola Particular
+6) Maior parte em Escola Particular com bolsa de estudos
+""",
+"""Qual a sua renda familiar? 
+1) Menos de 1 salário mínimo. (AtéR$1.320,00)
+2)1 a 2 salários mínimos.(De R$1.320 até R$2.640 00)
+3) 2 5 salários mínimos a 4. (De R$3.300 00 até R$5.280 00)
+4)Mais de 4 5 salários mínimos. (A partir de R$5.940 00) 
+""",
+"""Qual é a sua participação na vida econômica da sua família? 
+1)Não trabalho e meus gastos são custeados.
+2)Trabalho e sou independente financeiramente.
+3)Trabalhomas não sou independente financeiramente.
+4) Trabalho e sou responsável pelo sustento da minha família.
+5) Trabalho e contribuo com menos de 50% dos gastos familiares mensais. 
+""",
+"""Quantos smartphones existem em sua residência? 
+1)1 smartphone
+2) 2 smartphones
+3) 3 smartphones
+4) 4 smartphones ou mais
+5)Não há smartphones disponíveis 
+""",
+"""Quantos computadores/notebooks existem em sua residência?
+1) 1 computador/notebook
+2) 2 computadores/notebook
+3) 3 computadores/notebook
+4) 4 computadores/notebook ou mais
+5) Não há computadores/notebook na minha residência. 
+""",
+"""Você tem acesso a internet em sua residência?   
+1)sim
+2) não 
+""",
+"""Você acha que o roteiro educativo do novo ensino médio ajuda você a ter um melhor aprendizado?
+
+1) O roteiro educativo me ajuda muito a ter um melhor aprendizadopois me permite explorar as minhas áreas de interesse e me dá um conhecimento sólido.
+2) O roteiro educativo me ajuda razoavelmentepois ainda pode ser aprimorado para me ajudar a ter um melhor aprendizado.
+3) O roteiro educativo não me ajuda a ter um melhor aprendizadopois é muito superficial ou não me permite explorar as minhas áreas de interesse.
+4) O roteiro educativo não me ajuda em nada a ter um melhor aprendizadopois é muito ruim ou não me permite aprender nada.
+5) Não tenho certeza se o roteiro educativo me ajuda a ter um melhor aprendizado.
+6) Acredito que as mudanças não irão me ajudar a me preparar melhor para o mercado de trabalho. 
+""",
+"""Quais são os seus principais desafios em relação ao novo ensino médio? (Pode responder com mais de uma respostase desejar)
+
+1) Disciplinas indesejadas.
+2) Dificuldade de aprendizado.
+3) Pouca adaptação às metodologias de ensino.
+4) Aumento da carga horária.
+5) Redução de conteúdos.
+6) Redução de disciplinas de formação humana e crítica.
+7) Falta de professores qualificados. 
+""",
+"""Você acha a carga horária escolar exaustiva?
+
+1) Simacredito que minha carga horária é muito exaustiva.
+2) Simacredito que minha carga horária é moderadamente exaustiva.
+3) Simacredito que minha carga horária é ligeiramente exaustiva.
+4) Nãoacredito que minha carga horária não é exaustiva.
+5) O roteiro educativo não me ajuda em nada a ter um melhor aprendizadopois é muito ruim ou não me permite aprender nada.
+6) Não tenho certeza se o roteiro educativo me ajuda a ter um melhor aprendizado. 
+""",
+"""Você sente que os roteiros educativos levam em consideração as necessidades individuais dos alunos?
+
+1) Simacredito que os roteiros educativos levam em consideração as necessidades individuais dos alunos.
+2) Acredito que os roteiros educativos levam em consideração as necessidades individuais dos alunosmas de forma moderadapois ainda há algumas áreas que poderiam ser melhoradas.
+3) Acredito que os roteiros educativos levam em consideração as necessidades individuais dos alunosmas de forma levepois ainda há muitas áreas que poderiam ser melhoradas.
+4) Nãoacredito que os roteiros educativos não levam em consideração as necessidades individuais dos alunos.
+5) Não sei se os roteiros educativos levam em consideração as necessidades individuais dos alunos. 
+""",
+"""Você acredita que o novo ensino médio incentiva a participação dos alunos em atividades extracurriculares?
+
+1) Simacredito que o novo ensino médio incentiva a participação dos alunos em atividades extracurriculares.
+2) Acredito que o novo ensino médio incentiva a participação dos alunos em atividades extracurricularesmas de forma moderadapois ainda há algumas áreas que poderiam ser melhoradas.
+3) Acredito que o novo ensino médio incentiva a participação dos alunos em atividades extracurricularesmas de forma razoávelpois há muitas áreas que poderiam ser melhoradas.
+4) Nãoo novo ensino médio não incentiva a participação dos alunos em atividades extracurriculares.
+5) Não sei se o novo ensino médio incentiva a participação dos alunos em atividades extracurriculares. 
+""",
+"""Com relação ao trabalhoo que você acha das mudanças propostas no novo ensino médio?
+
+1) Acredito que as mudanças são muito positivaspois me ajudam a me preparar melhor para o mercado de trabalho.
+2) Acredito que as mudanças são moderadamente positivaspois me ajudam em alguns aspectos a me preparar melhor para mercado de trabalho.
+3) Acredito que as mudanças são razoavelmente positivaspois pouco me ajudam a me preparar melhor para o mercado de trabalho. 
+""",
+"""Você acha que os roteiros educativos podem ajudar os estudantes a escolherem carreiras mais alinhadas aos seus interesses?
+
+1) Acredito que os roteiros educativos podem me ajudar a escolher uma carreira mais alinhada aos meus interessespois eles me fornecem orientação valiosa sobre as diferentes  disponíveis e me ajudam a explorar minhas áreas de interesse.
+2) Acredito que os roteiros educativos podem me ajudar a escolher uma carreira mais alinhada aos meus interessesmas ainda precisa de melhorias.
+3) Eu não acho que os roteiros educativos tenham influência significativa na escolha de carreiramas eles podem ser úteis para fornecer uma visão geral das diferentes  disponíveis.
+4) Eu não acho que os roteiros educativos tenham influência na escolha de carreirapois são muito generalistas e não levam em conta as individualidades dos estudantes. 
+""",
+"""Você acredita que sua escola possui infraestrutura adequada para o novo ensino médio?
+
+1) Simacredito que minha escola possui infraestrutura totalmente adequada para o novo ensino médio.
+2) Acredito que minha escola possui infraestrutura moderadamente adequada para o novo ensino médiopois existem algumas áreas que poderiam ser melhoradas.
+3) Acredito que minha escola possui infraestrutura levemente adequada para o novo ensino médiopois existem muitas áreas que poderiam ser melhoradas.
+4) Nãominha escola não possui infraestrutura adequada para o novo ensino médio.
+5) Não tenho informações se minha escola possui infraestrutura adequada ao novo ensino médio. 
+""",
+"""Você acredita que sua escola possui estrutura pedagógica adequada para o novo ensino médio?
+
+1) Simacredito que minha escola possui estrutura pedagógica adequada para o novo ensino médio.
+2) Acredito que minha escola possui estrutura pedagógica moderadamente adequada para o novo ensino médiopois existem algumas áreas que poderiam ser melhoradas.
+3) Acredito que minha escola possui estrutura pedagógica levemente adequada para o novo ensino médiopois existem muitas áreas que poderiam ser melhoradas.
+4) Nãoacredito que minha escola não possui estrutura pedagógica adequada para o novo ensino médio.
+5) Não seipois não tenho informações se minha escola possui estrutura pedagógica adequada para o novo ensino médio. 
+""",
+"""Você acha que os professores estão se adaptando às mudanças no ensino médio?
+
+1) Acredito que estão se adaptando muito bemintegrando às mudanças com facilidade.
+2 ) Acredito que estão se adaptando moderadamentepois ainda há alguns desafios a serem superados.
+3) Acredito que estão se adaptando aos poucospois ainda há muita resistência por parte de alguns professores.
+4) Acredito que não estão se adaptandopois estão encontrando muitas dificuldades para se adaptar às mudanças.
+5) Não tenho certeza se os professores estão se adaptando às mudanças do novo ensino médio. 
+""",
+"""Em comparação com o antigo ensino médiovocê acredita que o atual é melhor que o antigo?
+
+1) Sim acredito que o atual ensino médio é melhor que o antigo.
+2) Acredito que o atual ensino médio é moderadamente melhor que o antigopois ainda há algumas áreas que poderiam ser melhoradas.
+3) Acredito que o atual ensino médio é levemente melhor que o antigopois ainda há muitas áreas que poderiam ser melhoradas.
+4)Nãoo atual ensino médio não é melhor que o antigo.
+5) Não sei se o novo ensino médio é melhor que o antigopois ainda estou me adaptando às mudanças. 
+"""]
